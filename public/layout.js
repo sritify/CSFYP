c07c0d9cf91c92f4002a0b105f7b491065139b48
nguz <angusso92331@gmail.com>
2014-05-05 23:26:40 +0800

    ui updated

---
+++ public/layout.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+		var window_h = $(window).height();
+		var window_w = $(window).width();
+		var control_h = $("#control").height();
+		var control_w = $("#control").width();
+		$('body').width(window_w).height(window_h*0.93);
+		$(".content").width(window_w*0.75).height((window_h-control_h)*0.93);
+		$(".sidebar2").width(window_w*0.246).height((window_h-control_h)*0.93);
+		$("#conversation").height(($(".sidebar2").height())-85);
+		$(window).resize(function(){
+			window_h = $(window).height();
+			window_w = $(window).width();
+			control_h = $("#control").height();
+			control_w = $("#control").width();
+			$('body').width(window_w).height(window_h*0.93);
+			$(".content").width(window_w*0.75).height((window_h-control_h)*0.93);
+			$(".sidebar2").width(window_w*0.246).height((window_h-control_h)*0.93);
+			$("#conversation").height(($(".sidebar2").height())-85);
+		})
+});(jQuery)
